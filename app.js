63b70cdee992396bc9e1b6211e988e4160c4cac0
Juan Thielmann <meganitrospeed@gmail.com>
2020-05-02 20:29:42 +0100

    Fixed CORS headers

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.set('view engine', 'pug');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
